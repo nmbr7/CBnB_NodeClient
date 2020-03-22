eccaad2e09e3313b99b342f76e5558167f14c31e
nmbr7 <suhailr7@ymail.com>
2020-03-22 13:41:44 +0000

    update

--- scripts/memusage.sh
+++ scripts/memusage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 use=$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes)
+cache=$(cat /sys/fs/cgroup/memory/memory.stat|head -n20|tail -n1|awk '{print $2}')
 limit=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
 
-echo "scale=10; $use/($limit/1024)*100" | bc
+echo "scale=10; ($use-$cache)/($limit)*100" | bc
+
 
